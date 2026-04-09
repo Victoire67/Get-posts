85623b54a9e6eb5260b5760b1b6e3da184968924
Victoire Ansima <ansimavicky@gmail.com>
2026-04-09 16:45:35 +0200

    ft : designed the header and fetcher components

---
+++ src/components/Fetcher.tsx
@@ -0,0 +1,17 @@
+export default function Fetcher() {
+  return (
+    <form action="" className="shadow-[0_0_15px_5px_rgba(0,0,0,0.1)] p-8 rounded-md">
+      <h1 className="text-xl font-bold">Fetch posts</h1>
+      <p className="text-gray-600">Enter the Api endpoint to fetch posts from </p>
+      <div className="flex gap-4">
+        {" "}
+        <input type="text" placeholder="url here" className="border-2 border-gray-100 rounded-sm outline-0 px-2 w-lg text-slate-600" />
+        <input
+          type="submit"
+          value={"Fetch"}
+          className=" bg-slate-900  p-1 rounded-sm text-white font-semibold cursor-pointer"
+        />
+      </div>
+    </form>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,6 @@
+export default function Header(){
+    return <header className="mb-12">
+        <h1 className="font-semibold text-4xl">Post Fetcher</h1>
+        <p className="text-gray-600">Enter the url to fetch and display posts</p>
+    </header>
+}
