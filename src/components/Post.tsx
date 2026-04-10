89089ea52f425f5b6dce4dabd1ee4463df27e74a
Victoire Ansima <ansimavicky@gmail.com>
2026-04-10 15:05:51 +0200

    ft : added spinner and the retry button

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -11,7 +11,7 @@ export default function PostElement({
   description,
 }: PostProps) {
   return (
-    <div className=" p-8 rounded-md w-2xl my-4 shadow-[0_0_15px_5px_rgba(0,0,0,0.1)]">
+    <div className=" p-8 rounded-md w-3xl my-4 shadow-[0_0_15px_5px_rgba(0,0,0,0.1)]">
       <h1 className="text-lg font-bold py-4">{postHeader}</h1>
       <p className="pb-2 opacity-50">
         <span>Post ID:{postId}</span> <span>User ID:{userId}</span>
--- src/getPost.ts
+++ src/getPost.ts
@@ -9,25 +9,33 @@ export type Post = {
 
 export default function usePost(url: string) {
     type PostsStates = [Post[], (prev: Post[]) => void]
-
+    let [isLoading, setIsloading] = useState(false)
     let postsStates: PostsStates = useState<Post[]>([])
 
     let posts = postsStates[0];
     const setPosts = postsStates[1];
 
-   
+
 
     let reqError: string = "Success ! no Error !"
 
     useEffect(() => {
         try {
+            setIsloading(true)
             fetch(url).then(result => result.json()).then(result => {
                 setPosts(result)
+                setIsloading(false)
+            }).catch(err => {
+                console.log("An error happend up !")
+                setIsloading(false)
+                throw new Error(err)
             })
         } catch (error) {
             reqError = error as string;
-
+            setIsloading(false)
+            console.log("An error happended")
+            throw new Error(reqError)
         }
     }, [url])
-    return [posts, setPosts , reqError]
+    return [posts, setPosts, reqError, isLoading]
 }
